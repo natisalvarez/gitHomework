6c8ee1755622feb977ea77cf039c349778667688
ivethglez80 <ivethglez80@gmail.com>
2023-07-31 15:02:33 -0300

    inicialize funcion

---
+++ index.js
@@ -0,0 +1,13 @@
+/* consigna:
+modificar la función del punto número seis (6) para que reciba como parámetro un número entero. 
+Si es un número par, debe convertirlo a binario y retornarlo,
+ y si es impar, convertirlo a hexadecimal y retornarlo. */
+
+ function laCajaDePandora(numero){
+    return numero%2===0 
+    ? /*a binario*/
+    : /*a hexadecimal*/
+ }
+
+ console.log(laCajaDePandora(10));
+ console.log(laCajaDePandora(7));
